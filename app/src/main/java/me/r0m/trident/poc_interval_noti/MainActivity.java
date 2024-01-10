80d3eb2efd0e0373298685a336fb24dd18104021
ろむねこ <nem@nem.x0.to>
2024-01-10 12:43:29 +0900

    WIP コード作成

--- app/src/main/java/me/r0m/trident/poc_interval_noti/MainActivity.java
+++ app/src/main/java/me/r0m/trident/poc_interval_noti/MainActivity.java
@@ -1,6 +1,12 @@
 package me.r0m.trident.poc_interval_noti;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +25,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TimePicker timePicker = findViewById(R.id.timePicker);
+        Button scheduleNotificationButton = findViewById(R.id.scheduleNotificationButton);
+
+        scheduleNotificationButton.setOnClickListener(v -> {
+            int hour = timePicker.getHour();
+            int minute = timePicker.getMinute();
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
+            calendar.set(Calendar.MINUTE, minute);
+            calendar.set(Calendar.SECOND, 0);
+
+            Intent intent = new Intent(MainActivity.this, NotificationReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
         });
     }
 }
